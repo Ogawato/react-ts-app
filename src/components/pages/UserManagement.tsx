2637ca3898c8bbd8d4f39298bd554cc4e249a59c
Ogawato <t-ogawa@yairilab.net>
2021-09-19 19:59:59 +0900

    ユーザー一覧画面の作成まで

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,6 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
-
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-    return <p>ユーザー管理画面ページです</p>
-})
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="tomoya"
+          fullName="goaway tomoya"
+        />
+      </WrapItem>
+    </Wrap>
+  );
+});
